c7bae3704d3901b01659b38b0e709244bc5359b6
Jeppe Pihl <jpihl08@gmail.com>
2019-03-04 13:13:01 +0100

    Closes: #1 Changed class from reply_broadcast_toggle to p-threads_footer__broadcast_checkbox - Thanks @aspiers

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMNodeInserted', function(e){
-    var nodes = e.relatedNode.getElementsByClassName('reply_broadcast_toggle');
+    var nodes = e.relatedNode.getElementsByClassName('p-threads_footer__broadcast_checkbox');
     Array.from(nodes).forEach(element => {
         console.log(element)
         element.checked = true;
