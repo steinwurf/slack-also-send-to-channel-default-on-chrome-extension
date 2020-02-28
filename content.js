57bdb99065c3935e537abd65a6e0fc4f634c5a6c
Jeppe Pihl <jpihl08@gmail.com>
2020-02-28 09:45:22 +0100

    Fixes #3

--- content.js
+++ content.js
@@ -5,27 +5,42 @@ document.addEventListener('DOMNodeInserted', function(e){
     if (containers.length == 0)
         return;
 
-    Array.from(containers).forEach(container => {
-        const editor = container.getElementsByClassName('ql-editor')[0];
-        if (editor === undefined)
-            return;
-        const checkbox = container.getElementsByClassName('p-threads_footer__broadcast_checkbox')[0];
-        if (checkbox === undefined)
-            return;
+    /// Add a short delay so that the sidebar has time to open.
+    setTimeout(() => {
+        Array.from(containers).forEach(container => {
+            const editor = container.getElementsByClassName('ql-editor')[0];
+            if (editor === undefined)
+            {
+                // console.log("SASTCDO: Unable to find editor");
+                return;
+            }
+            const checkbox = container.getElementsByClassName('p-threads_footer__broadcast_checkbox')[0];
+            if (checkbox === undefined)
+            {
+                // console.log("SASTCDO: Unable to find checkbox");
+                return;
+            }
 
-        const checkCheckbox = () => { if (!checkbox.checked) checkbox.click(); };
+            const checkCheckbox = () => {
+                // console.log("SASTCDO: Checking checkbox");
+                if (!checkbox.checked) {
+                    checkbox.click();
+                    // console.log("SASTCDO: checked");
+                }
+            };
 
-        // Initially the checkbox is disabled.
-        // Add an onclick listener to the message textbox which ticks the
-        // checkbox.
-        editor.onclick = checkCheckbox
+            // Initially the checkbox is disabled.
+            // Add an onclick listener to the message textbox which ticks the
+            // checkbox.
+            editor.onclick = checkCheckbox
 
-        // Try checking the checkbox after 500 ms so that the checkbox will be
-        // checked when sending multiple messages.
-        setTimeout(() => checkCheckbox(), 500);
+            // Try checking the checkbox after 500 ms so that the checkbox will be
+            // checked when sending multiple messages.
+            setTimeout(() => checkCheckbox(), 500);
 
-        // If the checkbox is clicked manually, disable the default to allow
-        // messages to not be sent to the channel
-        checkbox.onclick = () => {editor.onclick = undefined;};
-    });
+            // If the checkbox is clicked manually, disable the default to allow
+            // messages to not be sent to the channel
+            checkbox.onclick = () => {editor.onclick = undefined;};
+        });
+    }, 200);
 });
